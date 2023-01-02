282ec7fc736c96dbc376ac444ba4e810c7dcf548
SIRAMY Marc <marc.siramy@sully-group.fr>
2023-01-02 23:20:44 +0100

    project code

---
+++ src/main/java/com/baeldung/springintegration/controllers/TextAreaBean.java
@@ -0,0 +1,30 @@
+package com.baeldung.springintegration.controllers;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "textareaBean")
+@SessionScoped
+public class TextAreaBean {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String submit() {
+        if (message == null || message.isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Message is required"));
+            return null;
+        }
+        // Process the message
+        return "success";
+    }
+}
