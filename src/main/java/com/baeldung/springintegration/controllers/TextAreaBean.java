8033412402e320d2795c07e6fc6dc02fed6792a7
Marc Siramy <marc.siramy.external@atos.net>
2023-01-03 17:29:30 +0100

    update

--- src/main/java/com/baeldung/springintegration/controllers/TextAreaBean.java
+++ src/main/java/com/baeldung/springintegration/controllers/TextAreaBean.java
@@ -4,6 +4,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 
 @ManagedBean(name = "textareaBean")
 @SessionScoped
@@ -19,7 +20,7 @@ public class TextAreaBean {
         this.message = message;
     }
 
-    public String submit() {
+    public String submit(AjaxBehaviorEvent event) {
         if (message == null || message.isEmpty()) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Message is required"));
             return null;
